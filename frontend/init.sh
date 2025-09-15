efca6843c760303b1006f255aabc4fe746aef310
Jason Valdron <jason@valdron.ca>
2025-09-15 08:27:07 -0300

    Fix init.sh

--- frontend/init.sh
+++ frontend/init.sh
@@ -3,6 +3,6 @@
 CONFIG_FILE="/srv/assets/index-*.js"
 GRAPHQL_URL="${GRAPHQL_BASE_URL:-/graphql}"
 
-find /srv -name "index-*.js" -type f -exec sed -i "s|http://localhost:4000|${GRAPHQL_URL}|g" {} \;
+find /srv -name "index-*.js" -type f -exec sed -i "s|\(http\|ws\)://localhost:4000|${GRAPHQL_URL}|g" {} \;
 
 exec caddy run --config /etc/caddy/Caddyfile
