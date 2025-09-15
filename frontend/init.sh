3fc2ce1db04008955ed7d69b7f6847963a3119e1
Jason Valdron <jason@valdron.ca>
2025-09-15 20:19:50 -0300

    Fix subscriptions

--- frontend/init.sh
+++ frontend/init.sh
@@ -2,7 +2,8 @@
 
 CONFIG_FILE="/srv/assets/index-*.js"
 GRAPHQL_URL="${GRAPHQL_BASE_URL:-/graphql}"
+WS_URL="${WS_BASE_URL:-/subscriptions}"
 
-find /srv -name "index-*.js" -type f -exec sed -i -e "s|http://localhost:4000|${GRAPHQL_URL}|g" -e "s|ws://localhost:4000|${GRAPHQL_URL}|g" {} \;
+find /srv -name "index-*.js" -type f -exec sed -i -e "s|http://localhost:4000|${GRAPHQL_URL}|g" -e "s|ws://localhost:4000|${WS_URL}|g" {} \;
 
 exec caddy run --config /etc/caddy/Caddyfile
